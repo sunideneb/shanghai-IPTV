a3c812512d5c219ef2d2b305e690da6de64f50d3
yunalan <shzhb888@vip.qq.com>
2016-04-06 21:29:03 +0800

    command fix

--- 4k.sh
+++ 4k.sh
@@ -2,11 +2,10 @@ cd /jffs/ss/redchn
 rm -rf dnsmasq.postconf
 wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
 mv ss dnsmasq.postconf
-chomod -R 0755 dnsmasq.postconf
+chmod -R 0755 dnsmasq.postconf
 cd /jffs/scripts
 rm -rf dnsmasq.postconf
 wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/noss
 mv noss dnsmasq.postconf
-chomod -R 0755 dnsmasq.postconf
+chmod -R 0755 dnsmasq.postconf
 cd
-rm -rf 4k.sh
