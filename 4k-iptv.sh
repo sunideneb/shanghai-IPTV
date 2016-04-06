3ad9b23cce8dadbfecf599c64fc987be546a1c30
yunalan <shzhb888@vip.qq.com>
2016-04-06 21:39:26 +0800

    fix 2

--- 4k-iptv.sh
+++ 4k-iptv.sh
@@ -1,3 +1,6 @@
+cd /jffs
+wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/4-start
+chomod -R 0755 4k-start
 cd /jffs/ss/redchn
 rm -rf dnsmasq.postconf
 wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
@@ -8,4 +11,6 @@ rm -rf dnsmasq.postconf
 wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/noss
 mv noss dnsmasq.postconf
 chmod -R 0755 dnsmasq.postconf
+service restart_dnsmasq
 cd
+echo Finish!!! You Can Watch 4K IPTV In Lan4
