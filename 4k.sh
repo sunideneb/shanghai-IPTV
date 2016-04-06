1a5713b7e65ae33856611f436586c981b9824b2a
yunalan <shzhb888@vip.qq.com>
2016-04-06 21:26:40 +0800

    url update

--- 4k.sh
+++ 4k.sh
@@ -1,11 +1,11 @@
 cd /jffs/ss/redchn
 rm -rf dnsmasq.postconf
-wget --no-check-certificate https://sfdsgsg
+wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
 mv ss dnsmasq.postconf
 chomod -R 0755 dnsmasq.postconf
 cd /jffs/scripts
 rm -rf dnsmasq.postconf
-wget --no-check-certificate https://sfdsgsg
+wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/noss
 mv noss dnsmasq.postconf
 chomod -R 0755 dnsmasq.postconf
 cd
