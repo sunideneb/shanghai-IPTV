97d1dac816c7d1d259e076f9135c6aa384f542d8
yunalan <shzhb888@vip.qq.com>
2016-04-06 21:40:55 +0800

    fix 3

--- sh-4k-iptv.sh
+++ sh-4k-iptv.sh
@@ -1,4 +1,4 @@
-cd /jffs
+cd /jffs/scripts
 wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/4-start
 chomod -R 0755 4k-start
 cd /jffs/ss/redchn
