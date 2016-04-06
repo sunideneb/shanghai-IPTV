bf7f0c4a1659ef38a0676fdc3eec2b2e98c9f5fe
yunalan <shzhb888@vip.qq.com>
2016-04-06 21:24:59 +0800

    first
    first

---
+++ 4k.sh
@@ -0,0 +1,12 @@
+cd /jffs/ss/redchn
+rm -rf dnsmasq.postconf
+wget --no-check-certificate https://sfdsgsg
+mv ss dnsmasq.postconf
+chomod -R 0755 dnsmasq.postconf
+cd /jffs/scripts
+rm -rf dnsmasq.postconf
+wget --no-check-certificate https://sfdsgsg
+mv noss dnsmasq.postconf
+chomod -R 0755 dnsmasq.postconf
+cd
+rm -rf 4k.sh
